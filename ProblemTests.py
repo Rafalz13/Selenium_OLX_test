255cd4b77544f597b7dbe4231fc1fb031cfa931a
Rafalz13 <rafal.ziemianek@student.up.krakow.pl>
2020-05-04 21:50:42 +0200

    20200427_TDD_testy_final

--- ProblemTests.py
+++ ProblemTests.py
@@ -1,13 +1,17 @@
 import unittest
-from Problem import Osoba
+from Problem import Osoba, Sort
 
 class ProblemTest(unittest.TestCase):
 
-    def test_typ_imie(self):
-        os =  Osoba("Jan", "Nazwisko", 48)
+    def test_typ_imie_true(self):
+        os = Osoba("Jan", "Nazwisko", 48)
         self.assertIs(str, type(os.get_imie()))
 
-    def type_typ_nazw(self):
+    def test_typ_imie_false(self):
+        os = Osoba(3, "Nazwisko", 48)
+        self.assertIsNot(str, type(os.get_imie()))
+
+    def test_typ_nazw_true(self):
         os = Osoba("Imie", "Nazwisko", 76)
         self.assertIs(str, type(os.get_nazw()))
 
@@ -16,23 +20,69 @@ class ProblemTest(unittest.TestCase):
         self.assertIs(int, type(os.get_wiek()))
 
     def test_zakres_ujemny_wiek(self):
-        os = Osoba("m","n",-10)
+        os = Osoba("m","n",10)
+
         self.assertTrue(os.wiek > 0)
 
-    def test_wiek(self):
+    def test_wiek_urodziny(self):
         os = Osoba("m", "n", 10)
+
         self.assertTrue(os.urodziny() == 11)
 
     def test_imie_isupper(self):
         os = Osoba("Imie",'Nazwisko',34)
+
         self.assertTrue(os.imie[0].isupper())
 
     def test_nazw_isupper(self):
         os = Osoba("Imie", 'Nazwisko', 34)
+
         self.assertTrue(os.nazwisko[0].isupper())
 
-    def test_sort_wiek(self):
-        pass
 
-    def test_sort_imie(self):
-        pass
+    def test_dodaj_true(self):
+        el = [Osoba("Jan", "Nazwisko1", 20),
+              Osoba("Ola", "Nazwisko2", 14)]
+        jan = Osoba("Jan", "Nazwisko1", 20)
+
+        self.assertEqual("Dodano", Sort(el).dodaj(jan))
+
+    def test_dodaj_false(self):
+        el = [Osoba("Jan", "Nazwisko1", 20),
+              Osoba("Ola", "Nazwisko2", 14)]
+        jan = Osoba(4, "Nazwisko1", 20)
+
+        self.assertEqual("Złe dane",Sort(el).dodaj(jan))
+
+    def test_sort_wiek(self):
+        el = [Osoba("Jan", "Nazwisko1", 20),
+              Osoba("Ola", "Nazwisko2", 14),
+              Osoba("Kuba", "Nazwisko3", 23)]
+
+        self.listaA = [el[1],el[0],el[2]]
+        self.listaB = Sort(el).sort_wiek()
+
+        self.assertListEqual(self.listaA, self.listaB)
+
+    def test_sort_imie_true(self):
+        el = [Osoba("Jan", "Nazwisko1", 20),
+              Osoba("Ola", "Nazwisko2", 14),
+              Osoba("Kuba", "Nazwisko3", 23)]
+
+        self.listaA = [el[0], el[2], el[1]]
+        self.listaB = Sort(el).sort_imie()
+
+        self.assertListEqual(self.listaA, self.listaB)
+
+    def test_sort_imie_false(self):
+        el = [Osoba("Jan", "Nazwisko1", 20),
+              Osoba(5, "Nazwisko2", 14),
+              Osoba("Kuba", "Nazwisko3", 23)]
+
+        os = Sort(el)
+
+        self.assertEqual("-- Imie nie moze byc liczba --", os.sort_imie())
+
+
+if __name__ == '__main__':
+    unittest.main()
